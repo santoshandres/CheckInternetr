d463e83971732fccdb59c9cff6acb12dfea1ba2a
ranjith <ranjithstar256@gmail.com>
2018-07-05 17:08:21 +0530

    done

---
+++ app/src/main/java/info/androidhive/floatingview/MyApp.java
@@ -0,0 +1,51 @@
+package info.androidhive.floatingview;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+class MyApp {
+    public String getdetils(String s) {
+        String response = null;
+        try {
+            URL url = new URL(s);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            // read the response
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            response = convertStreamToString(in);
+        } catch (MalformedURLException e) {
+        } catch (ProtocolException e) {
+        } catch (IOException e) {
+        } catch (Exception e) {
+        }
+        return response;
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+}
--- app/src/main/java/info/androidhive/floatingview/MyService.java
+++ app/src/main/java/info/androidhive/floatingview/MyService.java
@@ -3,11 +3,15 @@ package info.androidhive.floatingview;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -16,30 +20,124 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MyService extends Service {
 
-    final Handler handler = new Handler();
-    private WindowManager mWindowManager;
-    private View mFloatingView;
-    WindowManager.LayoutParams params;
-    ImageView imageView;
+    private boolean isdataavailable() {
+        MyMusicc m = new MyMusicc();
+        m.execute();
+        return prefs.getBoolean("locked", false);
+    }
 
-    public MyService() {
+
+    public class MyMusicc extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            String  s1="nul";
+            try {
+                MyApp myApp = new MyApp();
+                String reslt = myApp.getdetils("http://echo.jsontest.com/key/value/one/two");
+                JSONObject jsonObject  = new JSONObject(reslt);
+               s1=jsonObject.getString("one");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return s1;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            //Log.i("djhgfg",j);
+            if (o.equals("two")) {
+                prefs.edit().putBoolean("locked", true).commit();
+            } else {
+                prefs.edit().putBoolean("locked", false).commit();
+            }
+        }
+    }
+
+//    private void initializeView(int y) {
+//        if (y==1){
+//                startService(new Intent(getApplicationContext(), FloatingViewService.class).putExtra("STATE",y));
+//        }else if(y==2) {
+//            startService(new Intent(getApplicationContext(), FloatingViewService.class).putExtra("STATE",y));
+//
+//        }
+//    }
+    //maybe you want to check it by getting the sharedpreferences. Use this instead if (locked)
+// if (prefs.getBoolean("locked", locked) {
+    // if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+//            mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widgettt, null);
+    //initializeView(2);
+    // Toast.makeText(MyService.this, "You are not connected to Internet", Toast.LENGTH_SHORT).show();
+    //mWindowManager.removeView(mFloatingView);
+    //mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+    //Add the view to the window.
+    //stopSelf();
+    //mWindowManager.removeView(mFloatingView);
+    // startService(new Intent(vv, FloatingViewService.class));
+    //           // initializeView(1);
+    //            // Toast.makeText(MyService.this, "You are connected to Internet", Toast.LENGTH_SHORT).show();
+    //            //Inflate the floating view layout we created
+    //            //stopSelf();
+    //            //mWindowManager.removeView(mFloatingView);
+
+
+
+
+
+
+    public void checkConnection() {
+        if (mFloatingView != null) {
+            if (isdataavailable()) {
+                imageView.setImageResource(R.drawable.green);
+            } else {
+                imageView.setImageResource(R.drawable.red);
+            }
+        } else {
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                checkConnection();
+                handler.postDelayed(this, 1000);
+            }
+        }, 1000);
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void onDestroy() {
+        handler.removeCallbacksAndMessages(null);
+        //if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+        //if (mFloatingView1 != null) mWindowManager.removeView(mFloatingView1);
+        Toast.makeText(this, "Service Stopped", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+
+    private boolean isViewCollapsed() {
+        return mFloatingView == null || mFloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        prefs = PreferenceManager.getDefaultSharedPreferences(MyService.this);
+
         mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
-       imageView=(ImageView) mFloatingView.findViewById(R.id.collapsed_iv);
+        imageView = (ImageView) mFloatingView.findViewById(R.id.collapsed_iv);
         imageView.setAlpha(50);
-         params = new WindowManager.LayoutParams(
+        params = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.TYPE_PHONE,
@@ -63,7 +161,9 @@ public class MyService extends Service {
         closeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                collapsedView.setVisibility(View.VISIBLE);
+                //collapsedView.setVisibility(View.VISIBLE);
+                //// if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
+                //stopSelf();
             }
         });
 
@@ -119,77 +219,24 @@ public class MyService extends Service {
         });
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
 
-        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                checkConnection();
-                handler.postDelayed(this, 1000);
-            }
-        }, 1000);
-        return super.onStartCommand(intent, flags, startId);
+
+    final Handler handler = new Handler();
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+    WindowManager.LayoutParams params;
+    ImageView imageView;
+   // String s1, s2;
+    SharedPreferences prefs;
+
+    public MyService() {
     }
 
     @Override
-    public void onDestroy() {
-        handler.removeCallbacksAndMessages(null);
-        //if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
-        //if (mFloatingView1 != null) mWindowManager.removeView(mFloatingView1);
-        Toast.makeText(this, "Service Stopped", Toast.LENGTH_SHORT).show();
-        super.onDestroy();
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    protected boolean isOnline() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    public void checkConnection() {
-        if (mFloatingView != null) {
-            if (isOnline()) {
-                imageView.setImageResource(R.drawable.green);
-            } else {
-                imageView.setImageResource(R.drawable.red);
-            }
-        }
-        else {
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-        }
-        // if (mFloatingView != null) mWindowManager.removeView(mFloatingView);
-//            mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widgettt, null);
-        //initializeView(2);
-        // Toast.makeText(MyService.this, "You are not connected to Internet", Toast.LENGTH_SHORT).show();
-        //mWindowManager.removeView(mFloatingView);
-        //mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
-        //Add the view to the window.
-        //stopSelf();
-        //mWindowManager.removeView(mFloatingView);
-        // startService(new Intent(vv, FloatingViewService.class));
-        //           // initializeView(1);
-        //            // Toast.makeText(MyService.this, "You are connected to Internet", Toast.LENGTH_SHORT).show();
-        //            //Inflate the floating view layout we created
-        //            //stopSelf();
-        //            //mWindowManager.removeView(mFloatingView);
-    }
-
-    private boolean isViewCollapsed() {
-        return mFloatingView == null || mFloatingView.findViewById(R.id.collapse_view).getVisibility() == View.VISIBLE;
-    }
-
-//    private void initializeView(int y) {
-//        if (y==1){
-//                startService(new Intent(getApplicationContext(), FloatingViewService.class).putExtra("STATE",y));
-//        }else if(y==2) {
-//            startService(new Intent(getApplicationContext(), FloatingViewService.class).putExtra("STATE",y));
-//
-//        }
-//    }
 }
